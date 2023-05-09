e63be5dfd78638dc87ee6460d0a7bb27ed067c72
Or Mahler <ormahler@Ors-MacBook-Pro.local>
2023-05-09 23:42:54 +0300

    remove global common

--- python_code/common/config_manager/load_config.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-
-def get_config(env):
-    with open(f"python_code/setup/config/{env}.yaml", "r") as yamlfile:
-        data = yaml.load(yamlfile, Loader=yaml.FullLoader)
-        print("Read successful")
-
-        return data
